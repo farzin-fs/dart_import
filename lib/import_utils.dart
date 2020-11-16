9fcde806590421503594e11f91a846493d2a5405
Farzin <farzin@firstsource.tech>
2020-11-16 11:10:35 +0330

    Better loop

--- lib/import_utils.dart
+++ lib/import_utils.dart
@@ -2,15 +2,20 @@ import 'utils.dart' as utils;
 import 'file_utils.dart' as file_utils;
 
 Future<List<String>> fixImports(List<String> lines, String path) async {
-  return await Future.wait(lines.map((line) async {
-    if (line.trim().startsWith("import ")) {
-      return await fixLine(line, path);
+  List<String> newLines = lines;
+  for (int i = 0; i < lines.length; i++) {
+    if (lines[i].trim().length == 0) {
+      continue;
     }
-    return line;
-  }));
+    if (!lines[i].trim().contains("import")) {
+      break;
+    }
+    newLines[i] = await fixImportLine(lines[i], path);
+  }
+  return newLines;
 }
 
-Future<String> fixLine(String line, String path) async {
+Future<String> fixImportLine(String line, String path) async {
   String packageName = 'package:${await utils.getPackageName()}';
   RegExp regex =
       RegExp('^\\s*import\\s*([\'"])$packageName/([^\'"]*)[\'"]([^;]*);\\s*\$');
