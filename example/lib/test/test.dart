d284015bcb8f01c9dfaf75a2ef9937681fe4022f
Farzin <farzin@firstsource.tech>
2020-11-16 12:27:16 +0330

    Add test file

---
+++ example/lib/test/test.dart
@@ -0,0 +1,17 @@
+import 'dart:convert';
+import 'dart:io';
+import 'package:flutter/material.dart';
+import 'dart:developer' as dev;
+import 'package:example/widgets/widget_a.dart';
+import 'package:example/utils.dart';
+import 'package:example/widgets/widget_b.dart';
+
+part 'part.dart';
+
+import 'package:example/main.dart';
+import 'package:example/widgets/widget_a.dart';
+import 'package:example/helpers/helper_b.dart';
+import 'package:example/helpers/helper_a.dart';
+
+
+import 'dart:async';
